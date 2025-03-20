7179b4c6b09b289e8a9a56e7c7b1c96496e2c4b2
VENU-25002 <venu25002@gmail.com>
2025-03-20 10:50:40 +0530

    sign page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,26 @@
-
-import Home from "./Home/Home";
-import StudentDashboard from "./Home/StudentDashboard";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Component/Home"; // Import the Home component
+import StudentDashboard from "./Component/StudentDashboard"; // Import the StudentDashboard component
+import Navbar from "./Component/Navbar"; // Import the Navbar component
+import SignIn from "./Component/SignIn"; // Import the SignIn page/component
+import ChooseUser from "./Component/ChooseUser"; // Import the ChooseUser component
 
 function App() {
-
   return (
-    <>
-    <Home/>
-    
-    </>
+    <Router>
+      {/* Include Navbar */}
+      
+
+      {/* Define Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Route for Home */}
+        <Route path="/student-dashboard" element={<StudentDashboard />} /> {/* Route for Student Dashboard */}
+        <Route path="/signin" element={<SignIn />} /> {/* Route for Sign In */}
+        <Route path="/choose-user" element={<ChooseUser />} /> {/* Route for ChooseUser */}
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
---
+++ my-react-app/src/Component/ChooseUser.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ChooseUserContainer, UserSection, Title, Button } from '../styles/ChooseUserStyles'; // Import styles
+import { Link } from 'react-router-dom';
+import './ChooseUser.css';
+
+
+const ChooseUser = () => {
+  return (
+    <ChooseUserContainer>
+      <UserSection>
+        <Title>Admin</Title>
+        <Link to="/admin-signIn">
+          <Button>Login as Admin</Button>
+        </Link>
+      </UserSection>
+      <UserSection>
+        <Title>Student</Title>
+        <Link to="/student-signIn">
+          <Button>Login as Student</Button>
+        </Link>
+      </UserSection>
+      <UserSection>
+        <Title>Teacher</Title>
+        <Link to="/teacher-signIn">
+          <Button>Login as Teacher</Button>
+        </Link>
+      </UserSection>
+    </ChooseUserContainer>
+  );
+};
+
+export default ChooseUser;
